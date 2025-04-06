c076c778ca993ec24016d8943fec139cfe661434
Eman <emanabukhater2001@gmail.com>
2025-04-06 17:09:08 +0300

    add Login Page

--- jw-auth-app/src/App.jsx
+++ jw-auth-app/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import { BrowserRouter,Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
 
 
 function App() {
   return (
-    <div>App</div>
+    <div>
+      <LoginPage />
+    </div>
   )
 }
 
---
+++ jw-auth-app/src/pages/LoginPage.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+function LoginPage({ login }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Call the login function passed as a prop
+    login(username, password);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+      />
+      <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default LoginPage;
