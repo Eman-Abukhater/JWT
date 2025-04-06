08572fbcdd3bca5ee1d2ba18c0d237a0638e4e6e
Eman <emanabukhater2001@gmail.com>
2025-04-06 19:24:44 +0300

    add the RBAC pages

---
+++ jw-auth-app/src/pages/AdminPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AdminPage() {
+  return (
+    <div>
+      <h2>Admin Page - Only for Administrators</h2>
+    </div>
+  )
+}
+
+export default AdminPage
---
+++ jw-auth-app/src/pages/UserPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function UserPage() {
+  return (
+    <div>
+      <h2>User Page - Only for Users</h2>
+    </div>
+  )
+}
+
+export default UserPage
